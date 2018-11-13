ff6bf4da2fd1a059b94b1177ef7ed310e87bc6c0
cthuff <craig@craighuff.com>
2018-11-12 19:07:53 -0800

    updated gsign-in

--- signin.php
+++ signin.php
@@ -2,7 +2,7 @@
 require_once 'vendor/autoload.php';
 
 // Get $id_token via HTTPS POST.
-
+$CLIENT_ID = "773465469592-70tepenvk2lc7sbhs1d1k5i98k0gdp09.apps.googleusercontent.com";
 $client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
 $payload = $client->verifyIdToken($id_token);
 if ($payload) {
