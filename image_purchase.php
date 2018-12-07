86395ea6140c5028f1196edd4e5391121de869f6
cthuff <craig@craighuff.com>
2018-12-06 20:53:55 -0800

    file rename

--- image_purchase.php
+++ image_purchase.php
@@ -1,19 +1,29 @@
 <?php
 include "../inc/dbinfo.inc";
 session_start();
+
+//Variables needed for purchasing image
 $filename = $_POST["filename"];
+$buyer_id = $_SESSION["$sql_id"];
+$path = "uploads/" . $filename;
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 // Check connection
 if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT user_id FROM UploadedImages WHERE image_name = '$filename';";
+$sql = "SELECT image_id, user_id FROM UploadedImages WHERE image_name = '$filename';";
 $results = $conn->query($sql);
 $row = $results->fetch_assoc();
-error_log($row["user_id"]);
+
+//Variables based on SQL call
+$uploader_id = $row['user_id'];
+
+$sql = "INSERT INTO PurchasedImages (image_id, publisher_id, buyer_id, image_name, filepath) values (, , , , );";
+mysqli_query($conn, $sql);
+
 try {
-    if ($row['user_id'] == $_SESSION["sql_id"]) {        
+    if ($uploader_id == $buyer_id) {        
 	throw new Exception('Cannot Purchase Your Own Photo', 300);
     }
     echo json_encode(array(
@@ -25,4 +35,5 @@ try {
         'error' =>  $e->getMessage(),
     ));
 }
+mysqli_close($conn);
 ?>
